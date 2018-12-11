af1e14c10909d02c09c762c34967a530cb2a62b4
mina98 <33330428+mina98@users.noreply.github.com>
2018-12-11 09:53:57 +0200

    fourth commite

---
+++ trace.py
@@ -0,0 +1,22 @@
+# import shapefile as shp
+# import matplotlib.pyplot as plt
+#
+# sf = shp.Reader("Police_bounds.shp")
+# for shape in sf.shapeRecords():
+#       print(shape.head())
+# #     x = [i[0] for i in shape.shape.points[:]]
+# #     y = [i[1] for i in shape.shape.points[:]]
+# #     color=['red','blue']
+# #     plt.plot(x,y,alpha=.7,color='#006400')
+# #     # plt.legend(bbox_to_anchor=(1, 1), loc=2, borderaxespad=0.)
+# # plt.show()
+# # sfe = shp.Reader("Police_points.shp")
+# #
+# # for shape in sfe.shapeRecords():
+# #     # print(shape.shape.points)
+# #     x = [i[0] for i in shape.shape.points[:]]
+# #     y = [i[1] for i in shape.shape.points[:]]
+# #
+# #     plt.plot(x,y,alpha=.7,color='#006400')
+# #     # plt.legend(bbox_to_anchor=(1, 1), loc=2, borderaxespad=0.)
+# # plt.show()
